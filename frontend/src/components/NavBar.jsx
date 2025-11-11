baa7b6deba2b56907d32510f37f0ff855b7bbe26
Roshan Karimi <Roshan.karimi@gmail.com>
2025-11-10 20:17:23 -0800

    dashboard navbar fixed

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,20 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./NavBar.css";
 
 export default function Navbar() {
+  const location = useLocation();
+  const onDashboard = location.pathname.toLowerCase() === "/dashboard";
+
+  console.log("Current path:", location.pathname);
+
   return (
     <nav className="navbar">
-      <div className="navbar-links">
-        <Link to="/Home">Home</Link>
-        <Link to="/Dashboard">Dashboard</Link>
-        <Link to="/Calendar">Calendar</Link>
-        <Link to="/Chores">Chores</Link>
-        <Link to="/Bills">Bills</Link>
-      </div>
+      {!onDashboard && (
+        <div className="navbar-links">
+          <Link to="/home">Home</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/calendar">Calendar</Link>
+          <Link to="/chores">Chores</Link>
+          <Link to="/bills">Bills</Link>
+        </div>
+      )}
 
       <div className="navbar-right">
-        <Link to="/Profile" className="user-profile">Profile</Link>
-        <Link to="/Login" className="navbar-signin">Sign In</Link>
+        <Link to="/profile" className="user-profile">Profile</Link>
+        <Link to="/login" className="navbar-signin">Sign In</Link>
       </div>
     </nav>
   );
