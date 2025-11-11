ca345f35d889eed21c5227f645a5d7c0b320e18a
mamikonyan, natalie <nataliemamikonyan@gmail.com>
2025-11-10 17:29:28 -0800

    created functions to change profile info

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -3,7 +3,8 @@ import Profile from "../functions/profile_fucntions.js"
 
 const router = Router(); // groups together requests
 
-router.post('/profile', async (req, res) => {
+// register a user
+router.post('/register', async (req, res) => {
   const { name, email, password, room_num } = req.body;
 
   if (!name || !email || !password || !room_num) return res.status(400).json({ error: "Missing value" });
@@ -15,6 +16,7 @@ router.post('/profile', async (req, res) => {
   res.status(201).json({ msg: "Successfully created profile" });
 });
 
+// select a profile
 router.get('/profile/:id', async (req, res) => {
   const { id } = req.params;
 
@@ -24,6 +26,7 @@ router.get('/profile/:id', async (req, res) => {
   res.status(200).json(result.profile);
 });
 
+// login a user
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
@@ -35,6 +38,50 @@ router.post('/login', async (req, res) => {
   if (!result.ok) return res.status(401).json({ error: result.error });
 
   res.status(200).json({message: 'Login successful'});
+});
+
+// change a users password
+router.post('/change/password', async (req, res) => {
+  const { userId, oldPass, newPass, confmPass } = req.body;
+
+  if (!userId || !oldPass || !newPass || !confmPass)
+    return res.status(400).json({ error: 'Missing required fields' });
+
+  const result = await Profile.changePassword(userId, oldPass, newPass, confmPass);
+
+  if (!result.ok) return res.status(400).json({ errors: result.errors });
+
+  res.status(200).json({ msg: result.msg });
+});
+
+// change a users email
+router.post('/change/email', async (req, res) => {
+  const { userId, newEmail, confmEmail } = req.body;
+
+  if (!userId || !newEmail || !confmEmail)
+    return res.status(400).json({ error: 'Missing required fields' });
+
+  const result = await Profile.changeEmail(userId, newEmail, confmEmail);
+
+  if (!result.ok)
+    return res.status(400).json({ errors: result.errors });
+
+  res.status(200).json({ msg: result.msg });
+});
+
+// change a users username
+router.post('/change/username', async (req, res) => {
+  const { userId, newUsername, confmUsername } = req.body;
+
+  if (!userId || !newUsername || !confmUsername)
+    return res.status(400).json({ error: 'Missing required fields' });
+
+  const result = await Profile.changeUsername(userId, newUsername, confmUsername);
+
+  if (!result.ok)
+    return res.status(400).json({ errors: result.errors });
+
+  res.status(200).json({ msg: result.msg });
 });
 
 
